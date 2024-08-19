a2b7f9202d740300678bafe5c9f34b039d600a5c
CARLOS EDUARDO FARIA MACIEL <carlos_e_maciel@estudante.sesisenai.org.br>
2024-08-19 17:07:44 -0300

    Adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='titulo1'>ola boa noite</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nostrum incidunt cum quibusdam? Assumenda error dolores reprehenderit commodi officia voluptatem ullam ipsa ut nam quos nulla, adipisci temporibus! Eaque, ad?</p>
+    </>
+  )
+}
+
+export default App
